2031279320777f7a92603fe579f7b3ba137ee217
lauyader2011@gmail.com <lauyader@127.0.0.1 localhost127.0.1.1 lauyader>
2015-11-20 17:55:46 -0430

    Mejorando el sensor de ultrasonido para activar motores DC

---
+++ Expor-github.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#echo "the $1 eats a $2 every time there is a $3"
+#echo "bye:-)"
+echo "hola mundo:"
+read var
+read -p 'enter'
+
+
+#git init
+#read -p "Presiona ENTER para continuar"
+#git add .
+#read -p "Presiona ENTER"
+#echo 'Escribe un comentario entre comillas (ejemplo: "Tu comentario"):'
+#read comentario
+#echo $comentario
+#git commit -m $comentario
+#git pull
+echo --- Fin del script del GitHub ----
---
+++ sensor_ultrasonido_Para_MotoresDC_PuenteH_mejorado/sensor_ultrasonido_Para_MotoresDC_PuenteH_mejorado.ino
@@ -0,0 +1,99 @@
+/*
+ HC-SR04 Ping distance sensor:
+ VCC to arduino 5v 
+ GND to arduino GND
+ Echo to Arduino pin 7 
+ Trig to Arduino pin 8
+ 
+ This sketch originates from Virtualmix: http://goo.gl/kJ8Gl
+ Has been modified by Winkle ink here: http://winkleink.blogspot.com.au/2012/05/arduino-hc-sr04-ultrasonic-distance.html
+ And modified further by ScottC here: http://arduinobasics.blogspot.com.au/2012/11/arduinobasics-hc-sr04-ultrasonic-sensor.html
+ on 10 Nov 2012.
+ */
+
+
+#define echoPin 5 // Echo Pin
+#define trigPin 4 // Trigger Pin
+#define LEDPin 7 // Onboard LED
+
+int maximumRange = 1000; // Maximum range needed
+int minimumRange = 0; // Minimum range needed
+long duration, distance; // Duration used to calculate distance
+
+int IN2 = 11;
+int IN1 =12;
+int IN3 = 10; 
+int IN4 = 9;
+
+
+void setup() {
+ Serial.begin (9600);
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
+ 
+ 
+   // Activacion de los pines de los motores
+  pinMode (IN4, OUTPUT);    // Input4 conectada al pin 9 
+  pinMode (IN3, OUTPUT);    // Input3 conectada al pin 10
+  pinMode (IN2, OUTPUT);    // Input2 conectada al pin 11
+  pinMode (IN1, OUTPUT);    // Input1 conectada al pin 12 
+  
+}
+
+void loop() {
+/* The following trigPin/echoPin cycle is used to determine the
+ distance of the nearest object by bouncing soundwaves off of it. */ 
+ digitalWrite(trigPin, LOW); 
+ delayMicroseconds(2); 
+
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10); 
+ 
+ digitalWrite(trigPin, LOW);
+ duration = pulseIn(echoPin, HIGH);
+ 
+ //Calculate the distance (in cm) based on the speed of sound.
+ distance = duration/58.2;
+ 
+ if (distance >= maximumRange || distance <= minimumRange){
+ /* Send a negative number to computer and Turn LED ON 
+ to indicate "out of range" */
+ Serial.println("-1");
+ digitalWrite(LEDPin, HIGH); 
+ Serial.begin(9600);
+ 
+ 
+  // Motor gira en un sentido
+  digitalWrite (IN4, HIGH);
+  digitalWrite (IN3, LOW); 
+  digitalWrite (IN2, HIGH);
+  digitalWrite (IN1, LOW);
+ }
+ else {
+ /* Send the distance to the computer using Serial protocol, and
+ turn LED OFF to indicate successful reading. */
+ Serial.println(distance);
+ digitalWrite(LEDPin, LOW);
+  if(distance >= 20) {
+   // Motor gira en un sentido
+  digitalWrite (IN4, HIGH);
+  digitalWrite (IN3, LOW); 
+  digitalWrite (IN2, HIGH);
+  digitalWrite (IN1, LOW);
+  
+  
+  digitalWrite(LEDPin, HIGH);
+  }
+  else
+  
+  // Motor no gira
+  digitalWrite (IN4, LOW);
+  digitalWrite (IN2, LOW); 
+ 
+  
+ }
+ 
+ // 50ms before next reading.
+ delay(50);
+}
