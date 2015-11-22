fc123f1916385045e3f5c260bf6458d6ed669672
Luis Americo Auyadermont Muñoz <lauyader2011@gmail.com>
2015-11-22 12:15:44 -0430

    Codigo que corrige la falla del Sensor de ultrasonido
     Las anteriores versiones, el sensor por el Serialprintln , cuando se mantenia a distancias superiores a la que es  capaz de reconocer y se mantenia en esa posición, Se neutralizaba, manteniendose en 0.
    Este código corrigio la falla. Agradecido a http://www.educachip.com/hc-sr04-arduino-tutorial/

--- SensorUltraSonidoFuncional/SensorUltraSonidoFuncional.ino
+++ SensorUltraSonidoFuncional/SensorUltraSonidoFuncional.ino
@@ -1,44 +1,57 @@
-/*
-Kiara Navarro
-sophiekovalevsky arroba fedoraproject dot org
-www.clase911.com
-*/
-#define echoPin 12
-#define triggerPin 11
-#define ledPin 13
-int delaymili = 500;
-int maximumRangeCm = 200;
-int minimumRangeCm = 0;
-long duration, distanceCm;
+// Las anteriores versiones, el sensor por el Serialprintln , cuando se mantenia a distancias superiores a la que es 
+// capaz de reconocer y se mantenia en esa posición, Se neutralizaba, manteniendose en 0.
+// Este código corrigio la falla. Agradecido a http://www.educachip.com/hc-sr04-arduino-tutorial/
+
+/*Pines a utilizar por Arduino. Puedes modificarlos por los pines digitales que desees*/
+
+int Trig=4;
+int Echo=3;
+
+//Variable en la que se va a almacenar el valor correspondiente a la distancia
+int Dist;
+
 void setup() {
 Serial.begin(9600);
-pinMode(triggerPin, OUTPUT);
-pinMode(echoPin, INPUT);
-pinMode(ledPin, OUTPUT);
+pinMode (Trig, OUTPUT);
+pinMode (Echo, INPUT);
+
 }
+
+//Este módulo calcula y devuelve la distancia en cm.
+/*
+Puedes poner el código del módulo directamente en el loop o utilizar el módulo
+para reducir el número de líneas de código del loop o reutilizar el código
+*/
+void ultrasonido (int &Distancia){
+
+//Para estabilizar el valor del pin Trig se establece a LOW
+digitalWrite (Trig, LOW);
+delay(10);
+
+//Se lanzan los 8 pulsos
+digitalWrite (Trig, HIGH);
+delay(10);
+digitalWrite (Trig, LOW);
+
+/*
+Se mide el tiempo que tarda la señal en regresar y se calcula la distancia.
+
+Observa que al realizar pulseIn el valor que se obtiene es tiempo, no distancia
+
+Se está reutilizando la variable Distancia.
+*/
+
+Distancia= pulseIn (Echo, HIGH);
+Distancia=Distancia/58;
+Serial.println(Distancia);
+Serial.println("Hola Mundo");
+delay(100);
+  
+}
+
 void loop() {
-initTrigger();
-duration = pulseIn(echoPin, HIGH);
-distanceCm = microsecCm(duration);
-if (distanceCm >= maximumRangeCm || distanceCm <= minimumRangeCm) {
-Serial.println("Out of range");
-Serial.begin(9600);
-digitalWrite(ledPin, HIGH);
-}
-else {
-digitalWrite(ledPin, LOW);
-Serial.print("Distance in cm: ");
-Serial.println(distanceCm);
-delay(delaymili);
-}
-}
-long microsecCm(long microsecond) {
-return microsecond / 58;
-}
-void initTrigger() {
-digitalWrite(triggerPin, LOW);
-delayMicroseconds(2);
-digitalWrite(triggerPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(triggerPin, LOW);
+
+ultrasonido (Dist);
+
+delay (250);
 }
