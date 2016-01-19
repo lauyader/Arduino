63fc4cf1ff8d0eb64933c68a41df7811df61a0d9
Luis Americo Auyadermont Muñoz <lauyader2011@gmail.com>
2016-01-19 19:15:01 -0430

    Script Mejorado y actualizado que funciona en el carro de yuruani
    Se mejoro el tiempo del sensor de ultrasonido HC-SR4

--- sensor_ultrasonido_Para_MotoresDC_PuenteH_mejorado/sensor_ultrasonido_Para_MotoresDC_PuenteH_mejorado.ino
+++ sensor_ultrasonido_Para_MotoresDC_PuenteH_mejorado/sensor_ultrasonido_Para_MotoresDC_PuenteH_mejorado.ino
@@ -1,127 +1,159 @@
 /*
  HC-SR04 Ping distance sensor:
- VCC to arduino 5v 
- GND to arduino GND
- Echo to Arduino pin 7 
- Trig to Arduino pin 8
+ /* 
+ Ejemplo de control de motor DC usando modulo L298
+ http://electronilab.co/tienda/driver-dual-para-motores-full-bridge-l298n/
  
- This sketch originates from Virtualmix: http://goo.gl/kJ8Gl
- Has been modified by Winkle ink here: http://winkleink.blogspot.com.au/2012/05/arduino-hc-sr04-ultrasonic-distance.html
- And modified further by ScottC here: http://arduinobasics.blogspot.com.au/2012/11/arduinobasics-hc-sr04-ultrasonic-sensor.html
- on 10 Nov 2012.
+ El programa activa el motor en un sentido por 4 segundos, 
+ para el motor por 500 ms, activa el motor en sentido inverso por 4 segundos 
+ y se detiene por 5 segundos. Luego repite la acción indefinidamente.
  
- Adaptado para arduino Nano:  Luis Américo Auyadermont Muñoz
- se le agrego un control de Puente H para controlar dos motores,
+ Creado 16/05/14
+ por Andres Cruz
+ ELECTRONILAB.CO
  */
-
-
-#define echoPin 3 // Echo Pin
-#define trigPin 2 // Trigger Pin
-#define LEDPin 13 // Onboard LED
-
-int maximumRange = 1000; // Maximum range needed
-int minimumRange = 0; // Minimum range needed
-float duration; // Duration used to calculate distance
-float distance;
-int IN2 = 7;
-int IN1 = 6;
-int IN3 = 5; 
-int IN4 = 4;
- 
-
-void setup() {
- Serial.begin (9600);
- pinMode(trigPin, OUTPUT);
- pinMode(echoPin, INPUT);
- pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
  
  
-   // Activacion de los pines de los motores
-  pinMode (IN4, OUTPUT);    // Input4 conectada al pin 4 
-  pinMode (IN3, OUTPUT);    // Input3 conectada al pin 5
-  pinMode (IN2, OUTPUT);    // Input2 conectada al pin 6
-  pinMode (IN1, OUTPUT);    // Input1 conectada al pin 7 
+ 
+// Variable del sensor ultrasonido 
+#define trigPin 7
+#define echoPin 6
+#define led 13
+#define led2 2
+ 
+
+int IN2 = 10;
+int IN1 =11;
+int IN3 = 9; 
+int IN4 = 8;
+
+void setup()
+{
   
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  /*
+  */ 
+  pinMode(led,OUTPUT);
+  pinMode(led2,OUTPUT);
   
- 
+  //Definir la salida del puente H
+  pinMode (IN4, OUTPUT);    // Input4 conectada al pin 9 
+  pinMode (IN3, OUTPUT);    // Input3 conectada al pin 10
+  pinMode (IN2, OUTPUT);    // Input2 conectada al pin 11
+  pinMode (IN1, OUTPUT);    // Input1 conectada al pin 12 
+  
 }
 
-void loop() {
-/* The following trigPin/echoPin cycle is used to determine the
- distance of the nearest object by bouncing soundwaves off of it. */ 
- digitalWrite(trigPin, LOW); 
- delayMicroseconds(2); 
+ 
 
- digitalWrite(trigPin, HIGH);
- delayMicroseconds(10); 
+
+
+void loop()
+{
+  
+  // Esperar por la conexion
+ // delay(3000);
  
- digitalWrite(trigPin, LOW);
- duration = pulseIn(echoPin, HIGH);
+   float duration, distance;
+
+  digitalWrite(trigPin, LOW);  // Added this line
+
+  delayMicroseconds(2); // Added this line
+
+  digitalWrite(trigPin, HIGH);
+
+  delayMicroseconds(1000); //- Removed this line
+
+//  delayMicroseconds(20); // Added this line
+
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = (duration/2) / 29.1;
+  Serial.println(distance); 
+
+   digitalWrite(led, LOW);
+   digitalWrite(led2, HIGH);
+  // CONDICIÓN CUANDO SE ENCUENTRA A MENOR DISTANCIA DEL OBSTACULO
+
+  if (distance < 5) {  // This is where the LED On/Off happens
+     
+     Serial.println(distance);
+    digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
+    digitalWrite(led2, LOW);
+         if (distance==0.00){
+               Serial.println("llego a cero");
+               digitalWrite(led, LOW);
+               digitalWrite(led2, LOW);
+                
+             }
+  
  
- //Calculate the distance (in cm) based on the speed of sound.
- distance = duration/58.2;
  
- if (distance >= maximumRange || distance <= minimumRange){
- /* Send a negative number to computer and Turn LED ON 
- to indicate "out of range" */
- 
- Serial.println("-1");
- Serial.println(distance);
- digitalWrite(LEDPin, HIGH); 
- //Serial.println("off");
- //Serial.begin (9600);
- 
- 
- /*
-  // Motor gira en un sentido
+        // Motor retrocede
   digitalWrite (IN4, HIGH);
-  digitalWrite (IN3, LOW); 
-  digitalWrite (IN2, HIGH);
-  digitalWrite (IN1, LOW);*/
- }
- else {
- /* Send the distance to the computer using Serial protocol, and
- turn LED OFF to indicate successful reading. */
- 
- Serial.println(distance);
- digitalWrite(LEDPin, LOW);
-  if(distance >= 10) {
-       
-       // Motor gira en un sentido contrario
-  digitalWrite (IN4, LOW);
-  digitalWrite (IN3, HIGH); 
-  digitalWrite (IN2, LOW);
-  digitalWrite (IN1, HIGH);
-  
-  digitalWrite(LEDPin, HIGH);
-  }
-  else
-  {
-  /*
-  // Motor no gira
-  digitalWrite (IN4, LOW);
-  digitalWrite (IN2, LOW); */
-  
-  
-
-  
-  
-    // Motor gira en un sentido
-
- 
-  
-  digitalWrite (IN4, HIGH);
-  digitalWrite (IN3, LOW); 
+  digitalWrite (IN3, LOW);
+  Serial.println("Menor distancia:");
+  Serial.println("Encender Motores traseros en retroceso");
+  //Cruzan las ruedas delanteras
   digitalWrite (IN2, HIGH);
   digitalWrite (IN1, LOW);
-  
+  delay(150);
+
+ 
+}
+
+  else {
+
+//    digitalWrite(led2,HIGH);
+    //digitalWrite(Pin, LOW);
+    
+      // Motor gira en un sentido hacia adelante Rueda de Empuje traseras
+      digitalWrite (IN4, LOW);
+      digitalWrite (IN3, HIGH); 
+      
+        //Cruzan las ruedas delanteras
+        
+        digitalWrite (IN2, LOW);
+        digitalWrite (IN1, HIGH);
+        
+        delay(150);
+        
+       // digitalWrite (IN2, LOW);
+       // digitalWrite (IN1, HIGH); 
   
   }
   
+/*
+  if (distance >= 100){
+    digitalWrite(led2, HIGH);
+
+    Serial.println("Out of range");
+    digitalWrite(trigPin, LOW);
+    
+    //digitalWrite(Pin, HIGH);
+    //pinMode(reset, OUTPUT);
+    //Serial.begin(9600);
+    //Serial.println("Reset");
+    //delay(200);
+    
+
+  }
+*/
+  /*else {
+
+    Serial.print(distance);
+
+    Serial.println(" cm");
+*/
+
+
+
+
+  delay(500);
  
-  
- }
- 
- // 50ms before next reading.
- delay(50);
 }
+
